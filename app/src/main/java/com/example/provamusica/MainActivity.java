dc4059c36fd12a549ce18eeaa7316e8a15342464
LeoXy03 <lbrenci91@gmail.com>
2022-01-19 12:44:32 +0100

    spinner e array adapter

--- app/src/main/java/com/example/provamusica/MainActivity.java
+++ app/src/main/java/com/example/provamusica/MainActivity.java
@@ -6,14 +6,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
     Button btnInsert;
     EditText edtSongName;
     GestoreBrani gb;
+    Spinner spnGeneri;
+    ArrayAdapter <String> aG;
 
+    String[] generi = {"hip pop", "trap", "rap"};
 
     @RequiresApi(api = Build.VERSION_CODES.O)
     @Override
@@ -22,12 +28,19 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         btnInsert = (Button) findViewById(R.id.invia);
         edtSongName = (EditText) findViewById(R.id.inserimento);
+        spnGeneri = (Spinner)findViewById(R.id.genere);
         gb = new GestoreBrani();
+        aG = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item,generi);
+        spnGeneri.setAdapter(aG);
         Brano b =new Brano("","","");
         btnInsert.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+
+                String genselezionato = spnGeneri.getSelectedItem().toString();
+                Toast.makeText(getApplicationContext(),genselezionato,Toast.LENGTH_LONG).show();
                 gb.addBrano(b);
+
             }
         });
     }
